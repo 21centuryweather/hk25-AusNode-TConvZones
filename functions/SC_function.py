5063ace33a8867d6b2488ec7b2b3b606bb6ffa2b
Sibyl Cheng <sc5932@gadi.org.nci.au>
2025-05-19 11:09:19 +1000

    Update selected notebooks and functions, ignore deleted files

--- functions/SC_function.py
+++ functions/SC_function.py
@@ -1,13 +1,8 @@
-
-from statsmodels.tsa.seasonal import STL
-from statsmodels.tsa.tsatools import detrend
-from scipy import stats
 import xarray as xr
 import numpy as np
 import healpy as hp
 import easygems.healpix as egh
-import matplotlib as mpl
-import matplotlib.pyplot as plt
+
 import cartopy.crs as ccrs
 
 def get_nn_lon_lat_index(nside, lons, lats):
@@ -22,3 +17,32 @@ def get_nn_lon_lat_index(nside, lons, lats):
         hp.ang2pix(nside, lons2, lats2, nest = True, lonlat = True),
         coords=[("lat", lats), ("lon", lons)],
     )
+    
+def doubel_itcz_index(lat_bounds , lon_bounds , data_all):
+
+    area_mean_Pn = [] ; area_mean_Ps = [] ;  area_mean_Pe =[]
+    area_mean = [area_mean_Pn , area_mean_Ps , area_mean_Pe]
+    for lat_pnse , d_mean in zip(lat_bounds , area_mean) :
+        
+        for data_c in data_all:
+            
+            lon_part1 = data_c.sel(latitude=lat_pnse, longitude=lon_bounds[0])
+            lon_part2 = data_c.sel(latitude=lat_pnse, longitude=lon_bounds[1])
+
+            # Combine both parts
+            subset = xr.concat([lon_part1, lon_part2], dim="longitude")
+            
+            weights = np.cos(np.deg2rad(subset['latitude']))
+
+        # Normalize the weights to sum to 1 across latitude
+            weights /= weights.sum()
+
+            # Apply weights across latitude
+            weighted_mean = subset.weighted(weights).mean(dim=['latitude', 'longitude'])
+            d_mean.append(weighted_mean.data )
+    
+    all_index = []
+    for i in range(5):
+        index = (area_mean_Pn[i] + area_mean_Ps[i])/2 - area_mean_Pe[i]
+        all_index.append(index)
+    return all_index
